cea6af1b99916055159dd8786448a5eb12fe91e3
sudheer <sudheerbabuganne@gmail.com>
2018-02-11 19:57:25 +0530

    First Commit

---
+++ SimpleJavaProject/src/exaples/FirstClass.java
@@ -0,0 +1,13 @@
+package exaples;
+
+public class FirstClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("first class-firstcheckout");
+
+	}
+
+}
